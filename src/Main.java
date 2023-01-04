2477261b898d779fe3a69060fd2987e9d0e15a7b
PraveenRoyal <30446334+PraveenRoyal@users.noreply.github.com>
2023-01-04 18:00:34 +0100

    just check

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+
+            Scanner scanner = new Scanner(System.in);
+
+            double max = 0;
+            double min = 0;
+
+            while (true) {
+
+                try {
+                    System.out.println("Please enter Number to run, or character to quit");
+
+                    double number = Double.parseDouble(scanner.nextLine());
+                    if (number < min) {
+                        min = number;
+                    }
+                    if (number > max) {
+                        max = number;
+                    }
+
+                } catch (NumberFormatException nfe) {
+                    break;
+                }
+            }
+
+            System.out.println("max = " + max);
+            System.out.println("min = " + min);
+
+        }
+
 }

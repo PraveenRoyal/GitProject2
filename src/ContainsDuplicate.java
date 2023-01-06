238ec14cb4a4a23d843a66a4168a536f30ae70e8
PraveenRoyal <30446334+PraveenRoyal@users.noreply.github.com>
2023-01-06 17:57:53 +0100

    nice

--- src/ContainsDuplicate.java
+++ src/ContainsDuplicate.java
@@ -5,16 +5,13 @@ public class ContainsDuplicate {
 
     public boolean containsDuplicate(int[] nums){
         int a = 0;
-        for(int i = 0; i<nums.length;i++){
-            for(int j = 0; j<nums.length;j++){
-                if(nums[i] == nums[j]){
+        for (int j : nums) {
+            for (int num : nums) {
+                if (j == num) {
                     a++;
                 }
             }
         }
-        if(a == nums.length){
-            return true;
-        }
-        return false;
+        return a == nums.length;
     }
 }
